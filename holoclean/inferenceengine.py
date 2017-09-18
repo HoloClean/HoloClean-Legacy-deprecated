124182f0af4a7e4f196d534dfd5b95b101eefd7a
Alireza <aheydari68@gmail.com>
2017-09-18 11:50:14 -0400

    The sparselr eliminated and InferenceEngine.py added

---
+++ holoclean/inferenceengine.py
@@ -0,0 +1,8 @@
+
+class InferenceEngine:
+    #Implement on the Tensorflow
+    pass
+#We then need connectors that will translate the SparseLR thing to Tensorflow and Pytorch
+class SparseLR:
+    #Implement on the Pytorch
+    pass
