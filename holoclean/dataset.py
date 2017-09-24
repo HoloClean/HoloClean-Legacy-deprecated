d04189328d91b886cfb88a54b2c7284bb77e2569
gmichalo <gmichalo@scspc709.cs.uwaterloo.ca>
2017-09-23 23:38:59 -0400

    setattribute and getattribute for dataset

--- holoclean/dataset.py
+++ holoclean/dataset.py
@@ -1,10 +1,15 @@
 #!/user/bin/env python
 class Dataset:
 
-	attributes = ['T', 'C_clean', 'C_dk', 'X', 'D', 'Y',
+	attributes = ['id','T', 'C_clean', 'C_dk', 'X', 'D', 'Y',
 					'W', 'b', 'Y_prod', 'config']
 	
 	def __init__(self):
 		self.attribute = {}
 		for a in Dataset.attributes:
 			self.attribute[a] = 0
+	def setatrribute(self,value,attr):
+		self.attribute[attr]=value
+		return self.attribute[attr]
+	def getattribute(self,attr):
+		return self.attribute[attr]
