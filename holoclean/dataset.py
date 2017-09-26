1a026ce9b3c23e9bb7bb1b22ff7f6bc2d8be2726
Alireza <aheydari68@gmail.com>
2017-09-26 14:17:42 -0400

    The dataset member tables description added

--- holoclean/dataset.py
+++ holoclean/dataset.py
@@ -6,6 +6,24 @@ class Dataset:
 
 	attributes = ['id','T', 'C_clean', 'C_dk', 'X', 'D', 'Y',
 					'W', 'b', 'Y_prod', 'config']
+	
+	"""
+	
+	Each element stand for some data that the holoclean needs or create:
+	
+		id : is the unique id for the dataset and it will be used in registering and retrieving data 
+		T : the initial data that get to the database from a file that user give
+		C_clean : it is table with index of clean cells 
+		C_dk : is table of indices that we don't know they are noisy or clean
+		X : table of feature vector each row of it is feature vector for a cell with id of indices it has size of cells in the T
+		D : is the table of domain for each attribute
+		Y : is the set of label for the cell in the T 
+		W : table of weights that we learn in the learning section
+		b : is the table that contains the biases that generated in learning part
+		Y_prod : is the table of probabilities for don't know cells  
+	
+	"""
+	
 	table_name=['']*len(attributes)
 	def __init__(self):
 		self.attribute = {}
