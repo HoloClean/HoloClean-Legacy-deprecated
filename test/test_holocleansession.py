97a394b96d1ddcda84b2c0d732fc8301eeabad6f
Alireza <aheydari68@gmail.com>
2017-10-02 06:09:58 -0400

    This is complete test for showing table in holoclean session

--- test/test_holocleansession.py
+++ test/test_holocleansession.py
@@ -7,7 +7,7 @@ from holoclean import holocleansession , dataset , dataengine
 my_path = os.path.abspath(os.path.dirname(__file__))
 path = os.path.join(my_path, "../holoclean/mysql-connector-java-5.1.44-bin.jar")
 holoclean_se=holocleansession.HolocleanSession(path)
-#spark_session=holoclean_se._start_spark_session(path)
+
 ds=dataset.Dataset()  
 d=dataengine.Dataengine("metadb-config.txt",'datadb-config.txt',ds,holoclean_se.return_sqlcontext())
 holoclean_se.set_dataengine(d)
