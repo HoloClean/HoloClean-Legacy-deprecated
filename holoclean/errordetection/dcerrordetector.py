7c0ca4ea29a78cba9739660c074c14f971a5ce51
Alireza <aheydari68@gmail.com>
2017-10-26 19:20:50 -0400

    pep8 consistance and commented

--- holoclean/errordetection/dcerrordetector.py
+++ holoclean/errordetection/dcerrordetector.py
@@ -1,4 +1,4 @@
-import sys 
+import sys
 sys.path.append('../../')
 from holoclean.utils.dcparser import DCParser
 
@@ -6,101 +6,104 @@ from holoclean.utils.dcparser import DCParser
 class DCErrorDetection:
     """TODO:
     This class return error
-    cells and clean 
-    cells based on the 
+    cells and clean
+    cells based on the
     denial constraint
     """
-    def __init__(self,DenialConstraints,dataengine,spark_session):
-        self.and_of_preds=DCParser(DenialConstraints).make_and_condition('all')
-        self.dataengine=dataengine
-        self.spark_session=spark_session
+    def __init__(self, DenialConstraints, dataengine, spark_session):
 
-    #Private methods
-    
-    def _index2list(self,dataset):        
+        """
+        This constructor at first convert all denial constraints
+        to the form of SQL constraints
+        and it get dataengine to connect to the database
+        """
+        self.and_of_preds = DCParser(DenialConstraints).make_and_condition('all')
+        self.dataengine = dataengine
+        self.spark_session = spark_session
+
+    # Private methods
+
+    def _index2list(self, dataset):
         """
         Returns list of indices
         :rtype: list[string]
         """
-        li_tmp=dataset.select('index').collect()
-        
-        return [list_count.asDict()['index'] for list_count in li_tmp ]   
-    
-    def _make_cells(self,tuples_dataframe,cond):
-        
-        """
-        This method create cell based on dataframe it get 
-        :param tuples_dataframe: spark_dataframe
-        :param cond: list[String]              
-        :rtype: spark_dataframe
-        """
-        
-        
-        all_list=self.dataengine.get_schema("Init")
-        all_list=all_list.split(',')
-        attr_list=DCParser.get_attribute(cond,all_list)
-        index_data=tuples_dataframe.select('ind').unionAll(tuples_dataframe.select('indexT2')).distinct()
-        dc_data=[]
-        for attribute in attr_list:
-            dc_data.append([attribute])
-        dc_df = self.spark_session.createDataFrame(dc_data,['attr'])
-        
-        result = index_data.crossJoin(dc_df)
-        
-        return result 
-    
-    def _violation_tuples(self,dataset):
+        li_tmp = dataset.select('index').collect()
+
+        return [list_count.asDict()['index'] for list_count in li_tmp]
+
+    def _make_cells(self, tuples_dataframe, cond):
 
         """
-        Return a list of two column dataframe that consist indices that create violation w.r.t. dc            
+        This method create cell based on dataframe it get
+        :param tuples_dataframe: spark_dataframe
+        :param cond: list[String]
+        :rtype: spark_dataframe
+        """
+
+        all_list = self.dataengine.get_schema("Init")
+        all_list = all_list.split(',')
+        attr_list = DCParser.get_attribute(cond, all_list)
+        index_data = tuples_dataframe.select('ind').unionAll(tuples_dataframe.select('indexT2')).distinct()
+        dc_data = []
+        for attribute in attr_list:
+            dc_data.append([attribute])
+        dc_df = self.spark_session.createDataFrame(dc_data, ['attr'])
+
+        result = index_data.crossJoin(dc_df)
+
+        return result
+
+    def _violation_tuples(self, dataset):
+
+        """
+        Return a list of two column dataframe that consist
+        indices that create violation w.r.t. dc
         :rtype: list[spark_dataframe]
         """
 
-        
-        dataset.createOrReplaceTempView("df") 
-        satisfied_tuples_index=[]
-        for cond in  self.and_of_preds: 
-            query="SELECT table1.index as ind,table2.index as indexT2 FROM df table1,df table2 WHERE ("+cond+")"        
-            satisfied_tuples_index.append(self.spark_session.sql(query))         
+        dataset.createOrReplaceTempView("df")
+        satisfied_tuples_index = []
+        for cond in self.and_of_preds:
+            query = "SELECT table1.index as ind,table2.index as indexT2 FROM df table1,df table2 WHERE ("+cond+")"
+            satisfied_tuples_index.append(self.spark_session.sql(query))
         return satisfied_tuples_index
-    
-    
-    # Setters  
-    
+
+    # Setters
+
     # Getters
-    
-    def get_noisy_cells(self,dataset):
-        
+
+    def get_noisy_cells(self, dataset):
+
         """
-        Return a dataframe that consist of index of noisy cells index,attribute             
+        Return a dataframe that consist of index of noisy cells index,attribute
         :rtype: spark_dataframe
         """
 
-        num_of_constarints= len(self.and_of_preds)
+        num_of_constarints = len(self.and_of_preds)
         violation = self._violation_tuples(dataset)
-        result=self._make_cells(violation[0],self.and_of_preds[0])
-        if num_of_constarints>1:
-            for dc_count in range(1,num_of_constarints):
-                result=result.unionAll(self._make_cells(violation[dc_count], self.and_of_preds[dc_count]))
-        return result.distinct()    
-    
-    def get_clean_cells(self,dataset,noisy_cells):
+        result = self._make_cells(violation[0], self.and_of_preds[0])
+        if num_of_constarints > 1:
+            for dc_count in range(1, num_of_constarints):
+                result = result.unionAll(self._make_cells(violation[dc_count], self.and_of_preds[dc_count]))
+        return result.distinct()
+
+    def get_clean_cells(self, dataset, noisy_cells):
 
         """
-        Return a dataframe that consist of index of clean cells index,attribute             
+        Return a dataframe that consist of index of clean cells index,attribute
         :rtype: spark_dataframe
         """
-
 
         dataset.createOrReplaceTempView("df")
-        q="SELECT table1.index as ind FROM df table1"
-        index_set=self.spark_session.sql(q)
-        all_attr=self.dataengine.get_schema("InitT").split(',')
-        rev_attr_list=[]
+        query = "SELECT table1.index as ind FROM df table1"
+        index_set = self.spark_session.sql(query)
+        all_attr = self.dataengine.get_schema("InitT").split(',')
+        rev_attr_list = []
         for attribute in all_attr:
             rev_attr_list.append([attribute])
-        all_attr_df = self.spark_session.createDataFrame(rev_attr_list,['attr'])
-        all_cell=index_set.crossJoin(all_attr_df)
-        
-        result =all_cell.subtract(noisy_cells) 
-        return result    
+        all_attr_df = self.spark_session.createDataFrame(rev_attr_list, ['attr'])
+        all_cell = index_set.crossJoin(all_attr_df)
+
+        result = all_cell.subtract(noisy_cells)
+        return result
--- holoclean/errordetection/errordetector.py
+++ holoclean/errordetection/errordetector.py
@@ -1,5 +1,3 @@
-import sys 
-sys.path.append('../')
 from dcerrordetector import DCErrorDetection
 
 
@@ -7,28 +5,29 @@ class ErrorDetectors:
     """TODO:
     This class call different error detection method that we needed
     """
-    def __init__(self,DenialConstraints,dataengine,spark_session,detection_type = None):
-        
-        if detection_type is None:
-            self.detect_obj=DCErrorDetection(DenialConstraints,dataengine,spark_session)
-    
-    #Setters:
-    
-    #Getters:
-      
-    def get_noisy_dknow_dataframe(self,data_dataframe):
-        
+    def __init__(self, DenialConstraints, dataengine, spark_session,
+            detection_type=None):
         """
-        Return tuple of noisy cells and clean cells dataframes             
+        In this class we instantiate a DC error detector and pass dataengine to
+        fill correspondence tables in data base
+        """
+        if detection_type is None:
+            self.detect_obj = DCErrorDetection(DenialConstraints,
+                                               engine, spark_session)
+
+    # Setters:
+
+    # Getters:
+
+    def get_noisy_dknow_dataframe(self, data_dataframe):
+
+        """
+        Return tuple of noisy cells and clean cells dataframes        
         :rtype: tuple[spark_dataframe]
         """
-        
-        noisy_cells=self.detect_obj.get_noisy_cells(data_dataframe)
-        clean_cells=self.detect_obj.get_clean_cells(data_dataframe,noisy_cells)
-        
-        return noisy_cells,clean_cells
- 
-	         
-        
 
-  
+        noisy_cells = self.detect_obj.get_noisy_cells(data_dataframe)
+        clean_cells = self.detect_obj.get_clean_cells(data_dataframe,
+                                                      noisy_cells)
+
+        return noisy_cells, clean_cells
