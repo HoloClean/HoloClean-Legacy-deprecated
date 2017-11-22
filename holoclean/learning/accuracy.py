333bf74de09b669354c085407d108ab071d080b3
gmichalo <gmichalo@scspc709.cs.uwaterloo.ca>
2017-11-21 19:11:17 -0500

    new class for accuracy

--- holoclean/learning/accuracy.py
+++ holoclean/learning/accuracy.py
@@ -5,19 +5,23 @@ class Accuracy:
         self.dataset=dataset
         self.path_to_grand_truth=path_to_grand_truth
         self.spark_session=spark_session
-        pass
 
-    def ingest(self):
-        pass
 
-    def accuracy_claculation(self):
+    def accuracy_calculation(self):
+	accuracy=0
+	dataframe1 = self.dataengine._table_to_dataframe("C_dk", self.dataset)
+        number_of_repairs = dataframe1.count()
+	df1 = self.dataengine._table_to_dataframe("Changes", self.dataset)
+        incorrect_repairs = df1.subtract(self.df).count()
 
-        pass
+        # We find the accuracy
+        accuracy = (1.0) * (number_of_repairs - incorrect_repairs) / number_of_repairs
+
+
 
     def read(self):
         """Create a dataframe from the csv file
 
         Takes as argument the full path name of the csv file and the spark_session
         """
-        df = self.spark_session.read.csv(self.path_to_grand_truth, header=True)
-        return df
+        self.df = self.spark_session.read.csv(self.path_to_grand_truth, header=True)
