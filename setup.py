4638e666a5bc728ad4b832d669a7104b3283e6c6
gmichalo <gmichalo@scspc709.cs.uwaterloo.ca>
2017-12-02 16:20:48 -0500

    setup.py for holoclean module

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version=__version__,
     packages=find_packages(),
     install_requires=['futures'],
-    package_data={'': ['holocelan/lib/mysql-connector-java-5.1.44-bin.jar']},
+    data_file=[('holoclean/lib/',['holocelan/lib/mysql-connector-java-5.1.44-bin.jar'])],
     include_package_data=True,
     entry_points={
         'console_scripts': [
