56ea2d6ba964288bb39e2440d0beb38929caf279
Alireza <aheydari68@gmail.com>
2017-12-07 15:17:24 -0500

    Interrupt for feature table size

--- holoclean/holoclean.py
+++ holoclean/holoclean.py
@@ -402,6 +402,7 @@ class Session:
                     global_counter = "select max(var_index) into @p from " + \
                         self.dataset.table_specific_name('Feature_init') + ";"
                     self.holo_env.dataengine.query(global_counter)
+                    raw_input("Count the Feature table")
 
         print ('adding weight_id to feature table...')
         self.holo_env.logger.info('adding weight_id to feature table...')
