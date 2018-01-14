1f322d30c270996649c4e63b2e1875f1a7583d02
j48zheng <matrixpachi@gmail.com>
2018-01-14 11:43:45 -0500

    fixed ground truth values

--- book-data/text2csv.bash
+++ book-data/text2csv.bash
@@ -2,8 +2,15 @@
 echo "Index,ISBN,Author_list" > book_golden.csv
 counter=0
 while read p; do
-	S="$(echo  "$p" | tr ',' '\@')"
-	echo "$counter,$S" | tr '\t' ','  >> book_golden.csv
-	
+	odd=$((counter % 2))
+	if [ $odd -eq 0 ]
+	then
+		S="$(echo  "$p" | tr ',' '\@')"
+		S="$(echo "$((counter/2)),$S" | tr '\t' ',')"
+		S=${S//';  '/','}
+		S=${S//' ,'/','}
+		S=${S//' ;'/';'}
+		echo "$S" | tr ' ' '_'  >> book_golden.csv
+	fi
 	counter=$((counter+1))
 done < book_golden.txt
