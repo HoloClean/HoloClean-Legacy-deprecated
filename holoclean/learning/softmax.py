8fcb77035d331bf52f706c6e787bde5b0e138c21
j48zheng <matrixpachi@gmail.com>
2018-02-08 10:43:05 -0500

    created X tensor in softmax holoclean

--- holoclean/learning/softmax.py
+++ holoclean/learning/softmax.py
@@ -10,4 +10,31 @@ class SoftMax:
         self.init_count=list[0].offset
         self.cooccur_count = list[1].offset
         self.DC_count =list[2].offset
+
+        # pytorch tensors
+        self.X = None
+        self._setupX()
+        return
+
+    # Will create the X-value tensor of size nxmxl
+    def _setupX(self):
+        coordinates = torch.LongTensor()
+        values = torch.LongTensor([])
+        feature_table = self.dataengine.get_table_to_dataframe("Feature", self.dataset).collect()
+        for factor in feature_table:
+            print(factor)
+            feature_index = None
+            if factor.TYPE == 'init':
+                feature_index = 0
+            elif factor.TYPE == 'cooccur':
+                feature_index = self.init_count + factor.feature - 1
+            elif factor.TYPE == 'FD':
+                feature_index = self.init_count + self.cooccur_count + factor.feature - 1
+
+            coordinate = torch.LongTensor([[factor.var_index - 1], [feature_index], [factor.assigned_val - 1]])
+            coordinates = torch.cat((coordinates, coordinate), 1)
+            value = factor['count']
+            values = torch.cat((values, torch.LongTensor([value])), 0)
+        self.X = torch.sparse.LongTensor(coordinates, values)
+        print(self.X.to_dense())
         return
