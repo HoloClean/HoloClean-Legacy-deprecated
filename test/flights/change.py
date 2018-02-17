2e2568ef52bdf8f38102f9731bf897bf279e050a
gmichalo <gmichalo@scspc709.cs.uwaterloo.ca>
2018-02-17 17:52:39 -0500

    adding unique values for flight_input

--- test/flights/change.py
+++ test/flights/change.py
@@ -1,8 +1,11 @@
 file1 = open("testfile.csv","w") 
 with open("flights_input.csv") as f:
     count = 0.00
+    index = 1
     list1 = []
     dict1 = {}
+    one = []
+    two = []
     list_onomata = ["sched_dep_time","act_dep_time","sched_arr_time","flight","act_arr_time"]
     for line in f:
         line1 = line.split(",")
@@ -14,7 +17,18 @@ with open("flights_input.csv") as f:
         else:
               list1 = []
               if count != 0:
-                  file1.write(count+","+dict1["flight"]+","+dict1["sched_dep_time"]+","+dict1["act_dep_time"]+","+dict1["sched_arr_time"]+","+dict1["act_arr_time"]+"\n")
+                  flights = dict1["flight"]
+                  if not (flights in one):
+                      one.append(dict1["flight"])
+                      file1.write(str(index) +","+dict1["flight"]+","+dict1["sched_dep_time"]+","+dict1["act_dep_time"]+","+dict1["sched_arr_time"]+","+dict1["act_arr_time"]+"\n")
+                      index = index +1
+                  else:
+                       if (float(one.index(flights)) % 20 > 0):
+                           file1.write(str(index) +","+dict1["flight"]+","+dict1["sched_dep_time"]+","+dict1["act_dep_time"]+","+dict1["sched_arr_time"]+","+dict1["act_arr_time"]+"\n")
+                           index = index +1
+                       #else:
+                       #    two.append(flights)
+                       #    print two
               dict1={}
               count = count1
               if line1[1] in list_onomata:
