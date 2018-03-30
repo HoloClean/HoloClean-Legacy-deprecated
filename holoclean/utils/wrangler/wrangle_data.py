ff768cda626fbfd91decc7e7dd9663dfd4cddb79
JVonderwell <jvonderwell@gmail.com>
2018-03-30 15:16:18 -0500

    added wrangling script

---
+++ holoclean/utils/wrangler/wrangle_data.py
@@ -0,0 +1,49 @@
+import click
+from pyspark.sql import SparkSession
+from transformer import Transformer
+from transform_functions import lowercase, trim
+from normalizer import Normalizer
+from col_norm_info import ColNormInfo
+import distance
+
+
+@click.command()
+@click.argument('path')
+@click.argument('out_path')
+def wrangle(path, out_path):
+
+    spark = SparkSession.builder.getOrCreate()
+
+    data = spark.read.csv(path, header=True, encoding='utf-8')
+
+    functions = [lowercase, trim]
+
+    # food cols
+    columns = ["city", "facilitytype"]
+
+    # hospital cols
+    #columns = data.columns
+
+    transformer = Transformer(functions, columns)
+
+    data = transformer.transform(data)
+
+    # food cols
+    cols_info = list()
+    cols_info.append(ColNormInfo("city"))
+    cols_info.append(ColNormInfo("facilitytype", distance.jaccard, 0.3))
+
+    # hospital cols
+    #for col in data.columns:
+    #    cols_info.append(ColNormInfo(col))
+
+    normalizer = Normalizer(cols_info)
+
+    data = normalizer.normalize(data)
+
+    data.toPandas().to_csv(out_path, index=False, header=True,
+                           encoding='utf-8')
+
+
+if __name__ == '__main__':
+    wrangle()
