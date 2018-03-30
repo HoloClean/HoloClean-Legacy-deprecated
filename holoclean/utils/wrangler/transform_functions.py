803a808ecd4f3e81f3583bc4330cb969bb035214
JVonderwell <jvonderwell@gmail.com>
2018-03-30 02:04:01 -0500

    added wrangler as module

---
+++ holoclean/utils/wrangler/transform_functions.py
@@ -0,0 +1,23 @@
+from pyspark.sql.functions import udf
+from pyspark.sql.types import StringType
+import re
+
+
+@udf(returnType=StringType())
+def lowercase(s):
+    if type(s) != str and type(s) != unicode:
+        return s
+    return str(s).lower()
+
+
+@udf(returnType=StringType())
+def trim(s):
+    if type(s) != str and type(s) != unicode:
+        return s
+    s = str(s)
+    s = s.lstrip()
+    s = s.rstrip()
+    s = re.sub(r"\n", '', s)
+    s = re.sub(r'"', '', s)
+    s = re.sub(r"'", '', s)
+    return s
