66436cbb98ad957e4d76fdf10b40d79ec9e7ffea
JVonderwell <jvonderwell@gmail.com>
2018-04-03 15:57:47 -0500

    changed one check

--- holoclean/utils/wrangler/transform_functions.py
+++ holoclean/utils/wrangler/transform_functions.py
@@ -13,7 +13,6 @@ def lowercase(s):
     return str(unicodedata.normalize('NFKD', s).encode('ASCII', 'ignore')).lower()
 
 
-
 @udf(returnType=StringType())
 def trim(s):
     if s is None:
@@ -23,12 +22,14 @@ def trim(s):
 
     if not isinstance(s, str):
         s = unicodedata.normalize('NFKD', s).encode('ASCII', 'ignore')
+
+    if s.isspace():
+        return ''
     s = s.lstrip()
     s = s.rstrip()
     s = re.sub("(\n)+", '', s)
     s = re.sub("(\s)+", ' ', s)
     s = re.sub("(\t)+", '', s)
-    s = re.sub(",(\s)*,", ',,', s)
     s = re.sub(r'"', '', s)
     s = re.sub(r"'", '', s)
     return s
