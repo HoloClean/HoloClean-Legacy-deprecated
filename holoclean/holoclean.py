777b79bbf9db45597088ce88bc2b391320a14868
gmichalo <gmichalo@scspc709.cs.uwaterloo.ca>
2018-04-29 21:39:21 -0400

    forked repo

--- holoclean/holoclean.py
+++ holoclean/holoclean.py
@@ -11,7 +11,7 @@ import torch
 from dataengine import DataEngine
 from dataset import Dataset
 from featurization.database_worker import DatabaseWorker, PopulateTensor
-from utils.cooccurpruning import CooccurPruning
+from utils.pruning import Pruning
 from utils.parser_interface import ParserInterface, DenialConstraint
 import multiprocessing
 
@@ -650,14 +650,10 @@ class Session:
             'starting domain pruning with threshold %s',
             pruning_threshold1)
 
-        self.pruning = CooccurPruning(
+        self.pruning = Pruning(
             self,
             pruning_threshold1, pruning_threshold2,
             pruning_dk_breakoff, pruning_clean_breakoff)
-
-        self.pruning.get_domain()
-
-        self.pruning._create_dataframe()
         self.holo_env.logger.info('Domain pruning is finished :')
         return
 
